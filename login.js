02a3fb2c4d656da12be3380cc84a26a58ea88e34
Srujan <vnsrujan@gmail.com>
2024-08-09 16:59:34 +0530

    SHHHOULLLLDDD ADDDDD JSSSS

--- login.js
+++ login.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the relevant elements
+
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
     const checkbox = document.querySelector(".cbox input");
 
-    // Predefined valid credentials
+
     const validEmail = "srujan@gmail.com";
     const validPassword = "1234";
 
-    // Function to handle login validation
+
     window.auth = function() {
         const username = usernameInput.value.trim();
+        console.log(username,"username")
         const password = passwordInput.value.trim();
 
         if (username === "") {
@@ -23,12 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
             return;
         }
 
-        // Check if the entered credentials are correct
+
         if (username === validEmail && password === validPassword) {
-            // Redirect to mode.html
             window.location.href = "mode.html";
         } else {
-            // Show error message
             if (username !== validEmail) {
                 alert("Wrong email.");
             } else {
@@ -37,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
 
-    // Function to toggle password visibility
     passwordInput.type = "password";
     checkbox.addEventListener("change", function() {
         if (checkbox.checked) {
--- name.js
+++ name.js
@@ -1,5 +1,13 @@
 const proceedButton = document.querySelector(".proceed");
+const firstPlayer = document.getElementById("input1");
+const secondPlayer = document.getElementById("input2");
 
 proceedButton.addEventListener("click", function() {
+    const firstPlayerName = firstPlayer.value;
+    const secondPlayerName = secondPlayer.value;
+    
+    localStorage.setItem("firstPlayerName", firstPlayerName);
+    localStorage.setItem("secondPlayerName", secondPlayerName);
+
     window.location.href = "game.html";
 });
